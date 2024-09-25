966c5ab5c079636b58632eb978a05d375864b668
Kaguya Shinomiya <kunciran1st@gmail.com>
2024-09-26 03:47:21 +0700

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,7 @@
+function printBanner() {
+  console.log("========== Tsubasa BOT ==========");
+  console.log("GitHub: https://github.com/kaguya1st");
+  console.log("Telegram: https://t.me/pumpbtcxyz");
+  console.log("===============================");
+}
+module.exports = printBanner;
