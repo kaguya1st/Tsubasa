ed5d2f1efdc7f619ab2543c2fa82c99f7e634c1e
Kaguya Shinomiya <kunciran1st@gmail.com>
2024-10-01 04:28:47 +0700

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -3,4 +3,5 @@ function printBanner() {
   console.log("GitHub: https://github.com/kaguya1st");
   console.log("Telegram: https://t.me/pumpbtcxyz");
   console.log(" ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠");
+}
 module.exports = printBanner;
