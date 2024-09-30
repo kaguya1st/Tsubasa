bd199b457acc32fba532f83297775ff4e637624a
Kaguya Shinomiya <kunciran1st@gmail.com>
2024-10-01 04:21:32 +0700

    Delete banner.js

--- banner.js
+++
@@ -1,6 +0,0 @@
-function printBanner() {
-  console.log("≠≠≠≠≠≠≠≠≠≠ Tsubasa BOT ≠≠≠≠≠≠≠≠≠≠≠≠≠");
-  console.log("GitHub: https://github.com/kaguya1st");
-  console.log("Telegram: https://t.me/pumpbtcxyz");
-  console.log(" ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠")
-module.exports = printBanner;
