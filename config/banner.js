f6f30f3de6a4d327c29f93b5f27ab3a36d22058b
Kaguya Shinomiya <kunciran1st@gmail.com>
2024-10-01 04:20:17 +0700

    Create banner.js

---
+++ config/banner.js
@@ -0,0 +1,6 @@
+function printBanner() {
+  console.log("≠≠≠≠≠≠≠≠≠≠ Tsubasa BOT ≠≠≠≠≠≠≠≠≠≠≠≠≠");
+  console.log("GitHub: https://github.com/kaguya1st");
+  console.log("Telegram: https://t.me/pumpbtcxyz");
+  console.log(" ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠")
+module.exports = printBanner;
